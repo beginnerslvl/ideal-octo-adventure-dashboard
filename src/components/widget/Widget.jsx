393ae27f520a1e47ce02e14ea47b994d4e5be328
Waleed Ajmal <127334056+Majboor@users.noreply.github.com>
2023-10-27 00:03:34 +0500

    Update Widget.jsx

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -8,25 +8,7 @@ import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlin
 import { useEffect, useState } from "react";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "../../firebase";
-import getUserPetName from "../../context/getUserPetName";
-
-function YourComponent() {
-  let var1 = ''; // Declare the variable to store the pet name
-
-  useEffect(() => {
-    const userIdentifier = "user@example.com"; // Replace with the actual user identifier
-    getUserPetName(userIdentifier)
-      .then((petName) => {
-        if (petName) {
-          var1 = petName; // Assign the pet name to var1
-          console.log("Pet name retrieved and assigned to var1:", var1);
-        }
-      })
-      .catch((error) => {
-        console.error("Error getting user's pet name:", error);
-      });
-  }, []);
-
+const var1 = 'random data';
 const Widget = ({ type }) => {
   const [amount, setAmount] = useState(null);
   const [diff, setDiff] = useState(null);
