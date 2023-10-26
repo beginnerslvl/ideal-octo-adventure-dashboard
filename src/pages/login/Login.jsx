e7ca2ad1e962712839f053964a3e27200565bd1a
Waleed Ajmal <127334056+Majboor@users.noreply.github.com>
2023-10-26 23:27:26 +0500

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,32 +3,53 @@ import "./login.scss";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
 import { useNavigate } from "react-router-dom";
-import {AuthContext} from "../../context/AuthContext"
+import { AuthContext } from "../../context/AuthContext";
+import { collection, addDoc } from "firebase/firestore"; // Import Firestore functions
 
 const Login = () => {
   const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState(""); // Add nickname state
+  const [petName, setPetName] = useState(""); // Add pet name state
 
-  const navitage = useNavigate()
+  const navigate = useNavigate();
 
-  const {dispatch} = useContext(AuthContext)
+  const { dispatch } = useContext(AuthContext);
 
-  // https://firebase.google.com/docs/auth/web/password-auth Docs for authentication both signup and login
-
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-        // we impport auth above bcuz it is required her 
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        dispatch({type:"LOGIN", payload:user})
-        navitage("/")
-      })
-      .catch((error) => {
-        setError(true);
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+
+      // Save the user profile data to Firestore
+      await addUserProfile(user.uid, nickname, petName);
+
+      dispatch({ type: "LOGIN", payload: user });
+      navigate("/");
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  // Function to add user profile data to Firestore
+  const addUserProfile = async (uid, nickname, petName) => {
+    // Reference the 'userProfiles' collection (assuming this is the collection name)
+    const userProfilesRef = collection(db, "userProfiles");
+
+    try {
+      // Add the user profile data to Firestore
+      await addDoc(userProfilesRef, {
+        uid,
+        email,
+        nickname,
+        petName,
       });
+    } catch (error) {
+      console.error("Error adding user profile: ", error);
+    }
   };
 
   return (
@@ -36,13 +57,23 @@ const Login = () => {
       <form onSubmit={handleLogin}>
         <input
           type="email"
-          placeholder="email"
+          placeholder="Email"
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
-          placeholder="password"
+          placeholder="Password"
           onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Nickname"
+          onChange={(e) => setNickname(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Pet Name"
+          onChange={(e) => setPetName(e.target.value)}
         />
         <button type="submit">Login</button>
         {error && <span>Wrong email or password!</span>}
